e78b3e8d7b266eb56be6459b98ab45e966295816
dougyster <dyang@college.harvard.edu>
2023-08-15 16:06:34 +0800

    added bridge

--- server/parser/filterdata.js
+++ server/parser/filterdata.js
@@ -11,11 +11,11 @@ fs.readFile('protocolData.json', 'utf8', (err, data) => {
     const jsonData = JSON.parse(data);
 
     // Filter and map data
-    const filteredCEXData = jsonData.filter(protocol => protocol.category == "CEX")
+    const filteredBridgeData = jsonData.filter(protocol => protocol.category == "Bridge")
                                  .map(protocol => ({name: protocol.name, logo: protocol.logo}));
 
     // Save filtered data back to a new JSON file
-    fs.writeFile('filteredCEXData.json', JSON.stringify(filteredCEXData), (err) => {
+    fs.writeFile('filteredBridgeData.json', JSON.stringify(filteredBridgeData), (err) => {
         if (err) throw err;
         console.log('Filtered data written to file');
     });
