082f4ffc87f45fc2ad9161a5250b51bfd33679dc
dougyster <dyang@college.harvard.edu>
2023-08-20 02:08:15 +0800

    reward calc

--- server/rewards/ratingMultiplier.js
+++ server/rewards/ratingMultiplier.js
@@ -30,7 +30,6 @@ function sigmoid(x) {
 
 // Step 3: Calculate Scores for Each Wallet
 // Caveat: Must delete all rankings between epochs
-const scoresByWallet = {};
 
 async function calculateScores(walletId, protocolData) {
   const temp = [];
@@ -46,9 +45,9 @@ async function calculateScores(walletId, protocolData) {
         // console.log("continuing")
         continue;
       }
-      console.log("user rating:", userRating.rating.scores)
-      console.log("user rating sum:", userRating.rating.scores[0])
-      console.log("protocol avgScore:", protocol.averageScore)
+      // console.log("user rating:", userRating.rating.scores)
+      // console.log("user rating sum:", userRating.rating.scores[0])
+      // console.log("protocol avgScore:", protocol.averageScore)
       // Calculate the distance for this protocol
       const dist = Math.sqrt(
         Math.pow(userRating.rating.scores[0] - protocol.averageScore, 2) + 
@@ -57,7 +56,7 @@ async function calculateScores(walletId, protocolData) {
         Math.pow(userRating.rating.scores[3] - protocol.averageScore, 2) + 
         Math.pow(userRating.rating.scores[4] - protocol.averageScore, 2)  
       );
-      console.log("dist:", dist)
+      // console.log("dist:", dist)
       // Add this distance to our temporary array
       temp.push(dist);
       
@@ -67,21 +66,30 @@ async function calculateScores(walletId, protocolData) {
   }
   
   // Compute the average of the distances for this wallet
-  const averageDist = temp.reduce((a, b) => a + b, 0) / temp.length;
+  if (temp.length == 0) {
+    console.log("No ratings")
+    return 0;
+  }
+  else {
+    const averageDist = temp.reduce((a, b) => a + b, 0) / temp.length;
+    // Store this average in the dictionary (object) under the current walletId
+    console.log("average dist:", averageDist)
+    console.log("sigmoid dist:", sigmoid(averageDist))
+    return sigmoid(averageDist);
+  }
   
-  // Store this average in the dictionary (object) under the current walletId
-  console.log("average dist:", averageDist)
-  console.log("sigmoid dist:", sigmoid(averageDist))
-  scoresByWallet[walletId] = sigmoid(averageDist);
 }
 
 
 // This will fetch protocol data, then calculate scores
-async function main() {
+async function main(userWallet) {
   const protocolData = await fetchProtocolData();
-  await calculateScores('0xa4775bAf967701d876CAAb5E1E55B695c608F2F0', protocolData);
-  console.log(scoresByWallet);
+  let res = await calculateScores(userWallet, protocolData);
+  return res
 }
 
-// Call the main function to start the process
-main();
+//export 
+module.exports = {
+  main
+};
+
--- server/rewards/trustlessStakeRewards.js
+++ server/rewards/trustlessStakeRewards.js
@@ -3,6 +3,7 @@ const TrustStaking = require('../../smart_contracts/artifacts/contracts/TRUSTSta
 const TrustStakingHelper = require('../../smart_contracts/artifacts/contracts/TRUSTStakingHelper.sol/TRUSTSTakingHelper.json');
 const { ethers } = require('ethers');
 const axios = require('axios');
+const { main } = require("./ratingMultiplier")
 
 // Instantiate the smart contracts with the admin address and sepolia instances
 const adminAddress = '0x8066221588691155A7594291273F417fa4de3CAe'
@@ -10,20 +11,43 @@ const adminSecretKey = '1f505417c00af3fb18d5d48afd892d8d318262e232d0fcba395a84ee
 const tokenAddress = "0x18260909b0ddc83326434bC303560aEaBf21A906";
 const stakingAddress = "0xd166F4001981984332EC0EE46E9a56421B1BaeA0";
 const helperAddress = '0x3f85109658b538112B224082612C33a52BC581aF';
+const INFURA_API_KEY = "634d5ea6a2e84ad18e28bd2119b17f93";
+const totalRewardAllocation = 10000000 //10 mil initial allocation
 
 //Instantiate the smart contracts with the admin address and sepolia instances
-const provider = new ethers.providers.JsonRpcProvider('https://rpc.sepolia.dev')
-const signer = new ethers.Wallet(adminSecretKey, provider)
-let trustContract = new ethers.Contract(tokenAddress, Trust.abi, signer);
-let trustStakingContract = new ethers.Contract(stakingAddress, TrustStaking.abi, signer);
-let trustStakingHelperContract = new ethers.Contract(helperAddress, TrustStakingHelper.abi, signer);
+async function setupContracts() {
+    const provider = new ethers.providers.JsonRpcProvider(`https://sepolia.infura.io/v3/${INFURA_API_KEY}`)
+    await provider.ready
+    // console.log("Provider:", provider)
+    const signer = new ethers.Wallet(adminSecretKey, provider)
+    let trustContract = new ethers.Contract(tokenAddress, Trust.abi, signer);
+    let trustStakingContract = new ethers.Contract(stakingAddress, TrustStaking.abi, signer);
+    let trustStakingHelperContract = new ethers.Contract(helperAddress, TrustStakingHelper.abi, signer);
+    await Promise.all([
+        trustContract.ready,
+        trustStakingContract.ready,
+        trustStakingHelperContract.ready  
+      ])
+    // console.log("TrustStakingContract:", trustStakingContract)
+    return [trustContract, trustStakingContract, trustStakingHelperContract]
+}
 
 //Calculate %staked for a given user
-async function calculateStakedPercentage(userAddress) {
-    let totalStaked = await trustContract.balanceOf(helperAddress);
-    let userStaked = await trustStakingHelperContract.viewStakeByAddress(userAddress);
-    let percentageStaked = userStaked / totalStaked;
-    return percentageStaked;
+let stakingRewards = {};
+async function calculateStakedPercentage(userWallets, trustContract, trustStakingHelperContract) {
+    for (const userWallet of userWallets) {
+
+        console.log("User:", userWallet);
+
+        let totalStaked = Number(ethers.utils.formatUnits(await trustContract.balanceOf(helperAddress), 18));
+
+        let userStaked = Number(ethers.utils.formatUnits(await trustStakingHelperContract.viewStakeByAddress(userWallet),18)); 
+
+        let percentageStaked = userStaked / totalStaked;
+        console.log("percentageStaked:", percentageStaked);
+
+        stakingRewards[userWallet] = totalRewardAllocation * percentageStaked;
+    }
 }
 
 //Get all walletIdss
@@ -39,11 +63,18 @@ async function fetchAllWalletIds() {
     }
 }
 
-async function main(){
+// adding rating multipliers for normal rating rewards
+async function rewards(){
+    let [trustContract, trustStakingContract, trustStakingHelperContract] = await setupContracts()
     output = await fetchAllWalletIds()
-    console.log("output:", output)
+    await calculateStakedPercentage(output, trustContract, trustStakingHelperContract);
+    console.log("calculating rating multiplier")
+    for (const userWallet in stakingRewards) {
+        stakingRewards[userWallet] = Math.ceil(stakingRewards[userWallet] * await main(userWallet))
+        console.log("staking reward for ", userWallet, ":", stakingRewards[userWallet])
+    }
 }
 
-main()
+rewards()
 
 
